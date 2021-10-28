cc0fb342908c604587b06d19c443df31fa8f9bd1
ujjwol75 <ujjwolneupane75@gmail.com>
2021-10-28 19:39:01 +0545

    initial commit

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('courses', views.courses, name='courses'),
+    path('bsc-csit', views.bsccsit, name='bsccsit'),
+    path('semester', views.semester, name='semester'),
+    path('subject', views.subject, name='subject'),
+    path('model-questions', views.model_questions, name='model-questions'),
+    path('refrenced-books', views.refrenced_books, name='refrenced-books'),
+    path('tech-news', views.tech_news, name='tech-news'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+
+# ===========course view===========
+def courses(request):
+    return render(request, 'app/courses.html')
+
+# =============BSC CSIT================
+def bsccsit(request):
+    return render(request, 'app/bsc-csit.html')
+
+# =============SEMESTER================
+def semester(request):
+    return render(request, 'app/semester.html')
+
+# =============SUBJECT================
+def subject(request):
+    return render(request, 'app/subject.html')
+
+# =============MODEL QUESTIONS================
+def model_questions(request):
+    return render(request, 'app/model-questions.html')
+
+# =============REFRENCED BOOKS================
+def refrenced_books(request):
+    return render(request, 'app/refrenced-books.html')
+
+# =============TECH NEWS================
+def tech_news(request):
+    return render(request, 'app/tech-news.html')
